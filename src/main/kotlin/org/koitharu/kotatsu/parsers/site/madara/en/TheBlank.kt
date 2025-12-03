f29cf7c416741d7d2612d7ff0b4d99c9a4d62bcb
dragonx943 <premieregirl26@gmail.com>
2025-12-03 21:07:01 +0700

    chore(TheBlank): Add broken annotation

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/TheBlank.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/TheBlank.kt
@@ -1,11 +1,13 @@
 package org.koitharu.kotatsu.parsers.site.madara.en
 
+import org.koitharu.kotatsu.parsers.Broken
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
 import org.koitharu.kotatsu.parsers.model.ContentType
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
+@Broken("Need to rewrite the parser")
 @MangaSourceParser("THEBLANK", "TheBlank", "en", ContentType.HENTAI)
 internal class TheBlank(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.THEBLANK, "theblank.net") {
